bd51cc8a6696b2fa4aef23ce687fd553997d2996
Tobias Rasmussen <tobulf@gmail.com>
2019-12-29 21:32:03 +0100

    Restructure loop for different types of callback

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ using namespace std;
 
 Serial pc(USBTX, USBRX, 115200);
 InterruptIn button(PC_13);
-DigitalOut ouput_pin(PC_4);
+DigitalOut output_pin(PC_4);
 const uint8_t MAC[6] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x06};
 UipEthernet net(MAC, D11, D12, D13, D10); // mosi, miso, sck, cs
 TcpServer server;                         // Ethernet server
@@ -51,21 +51,33 @@ uint8_t uplink_GW_t;
 string uplink_port;
 string uplink_devEUI;
 string uplink_payload;
+uint8_t uplink_payload_bytes[20];
 
 uint32_t airtime;
+ uint64_t temp;
 
 const int NTP_PACKET_SIZE = 48;
 uint8_t NTP_packetBuffer[NTP_PACKET_SIZE];
-const uint16_t DOWNLINK_PACKET_SIZE = 125;
+const uint16_t DOWNLINK_PACKET_SIZE = 140;
 uint8_t Downlink_packetBuffer[DOWNLINK_PACKET_SIZE];
 
 Watchdog &watchdog = Watchdog::get_instance();
 
 string callback_msg;
 
+enum callbackType {SYNC=1,EVENT=2};
+
 void printfunc()
 {
+    temp = RTC_us.read_high_resolution_us() + epoch_us;
     print_time = true;
+    if (output_pin.read()){
+       output_pin.write(0);
+    }
+    else{
+        output_pin.write(1);
+    }
+    
 }
 
 void SyncEpoch()
@@ -184,6 +196,24 @@ void parseJSON(Json* jsonOBJ){
     strncpy ( payloadValue, payloadStart, payloadLength );
     payloadValue[ payloadLength ] = 0;
     uplink_payload = string(payloadValue);
+    uplink_payload = uplink_payload.substr(1, uplink_payload.size() - 2);
+    uint8_t i = 0;
+    uint8_t num = 0;
+    uint8_t num_len = 0;
+    uint8_t start = 0;
+    uint8_t length = uplink_payload.length();
+    while(i <= length){
+        if((uint8_t)uplink_payload[i] == 44 || i == length){
+            uplink_payload_bytes[num] = stoi(uplink_payload.substr(start, num_len));
+            num++;
+            start = i+1;
+            num_len = 0;
+        }
+        else{
+            num_len++;
+        }
+        i++;
+    }
 
     payloadStart  = jsonOBJ->tokenAddress (14);
     payloadLength = jsonOBJ->tokenLength (14);
@@ -227,12 +257,97 @@ string createCallbackObject(uint64_t T_sync, uint32_t T_callback){
     ","+to_string((uint8_t)(T_callback & 0xFF))+"]} ";
     return callbackMsg;
 };
-/**
- * @brief
- * @note
- * @param
- * @retval
- */
+
+bool synchronize_callback(IpAddress HostIP){
+            string callbackJson;
+            int garbadge_length = (DOWNLINK_PACKET_SIZE+callbackJson.length())/2;
+            uint8_t garbadge[garbadge_length];
+            socket.flush();
+            socket.beginPacket(HostIP, TX_RTT_port);
+            socket.write((const uint8_t*)garbadge,garbadge_length);
+            watchdog.kick();
+            if(!socket.endPacket()){
+                #ifdef DEBUG
+                printf("something went wrong sending UDP RTT... \r\n");
+                #endif
+                Callback_timer.stop();
+                Callback_timer.reset();
+                socket.flush();
+                socket.close();
+                socket.begin(UL_port);
+                watchdog.kick();
+                return false;
+            }
+            RTT_timer.start();
+            socket.flush();
+            socket.close();
+            socket.begin(RX_RTT_port);
+            while(!socket.parsePacket());
+            socket.read(Downlink_packetBuffer, DOWNLINK_PACKET_SIZE);
+            socket.flush();
+            watchdog.kick();
+            RTT_timer.stop();
+            Callback_timer.stop();
+            uint64_t timestamp = RTC_us.read_high_resolution_us()
+            + epoch_us;
+            uint32_t T_callback = Callback_timer.read_high_resolution_us() + 
+            RTT_timer.read_high_resolution_us() + ((uint32_t)uplink_GW_t*1000) +
+            airtime;
+            callbackJson = createCallbackObject(timestamp, T_callback);
+            //#ifdef DEBUG
+            uint32_t temp_sec = timestamp / 1000000;
+            uint32_t temp_us = timestamp - ((uint64_t)temp_sec * 1000000);
+            uint16_t temp_ms = temp_us/1000;
+            temp_us = temp_us - (uint32_t)temp_ms*1000;
+            printf("Seconds since January 1, 1970 = %u ms: %u us: %u \r\n",
+            (unsigned int)temp_sec, (unsigned int)temp_ms, (unsigned int)temp_us);
+            printf("RTT: %d \r\n",RTT_timer.read_us());
+            printf("Tot callback time: %u us airtime %u \r\n", T_callback, airtime);
+            //#endif
+            RTT_timer.reset();
+            watchdog.kick();
+            socket.beginPacket(HostIP, DL_port);
+            socket.write((const uint8_t*) callbackJson.c_str(), callbackJson.length());
+            if(!socket.endPacket()){
+                printf("something went wrong sending UDP Uplink... \r\n");
+                Callback_timer.stop();
+                Callback_timer.reset();
+                socket.flush();
+                socket.close();
+                socket.begin(UL_port);
+                watchdog.kick();
+                return false;
+            };
+            watchdog.kick();
+            socket.flush();
+            socket.close();
+            socket.begin(UL_port);
+            Callback_timer.stop();
+            #ifdef DEBUG
+            printf("Callback time: %d us\r\n", Callback_timer.read_us());
+            #endif
+            char buffer[32];
+            time_t seconds = time(NULL);
+            strftime(buffer, 32, "%H:%M:%S %p\n", localtime(&seconds));
+            printf("Uplink received from %s \r\n", uplink_devEUI.c_str());
+            printf("Time GMT0: %s \r", buffer);
+            Callback_timer.reset();
+            return true;
+}
+bool event_callback(IpAddress HostIP){
+    uint64_t timestamp = (uint64_t)uplink_payload_bytes[1] << 48 |
+    (uint64_t)uplink_payload_bytes[2] << 40 |
+    (uint64_t)uplink_payload_bytes[3] << 32 |
+    (uint64_t)uplink_payload_bytes[4] << 24 |
+    (uint64_t)uplink_payload_bytes[5] << 16 |
+    (uint64_t)uplink_payload_bytes[6] << 8 |
+    (uint64_t)uplink_payload_bytes[7];
+    uint32_t temp_sec = timestamp / 1000000;
+    uint32_t temp_us = timestamp - ((uint64_t)temp_sec * 1000000);
+    printf("Event occured @ %u, us: %u \r\n",
+    (unsigned int)temp_sec, (unsigned int)temp_us);
+    return false;
+}
 
 int main(void)
 {
@@ -266,32 +381,24 @@ int main(void)
     while (true)
     {
         if(socket.parsePacket()){
-            
             Callback_timer.start();
+            watchdog.kick();
             socket.read(Downlink_packetBuffer, DOWNLINK_PACKET_SIZE);
             HostIP = socket.remoteIP();
-            uint32_t hostAdress =  (uint32_t)HostIP.rawAddress();
-            #ifdef DEBUG
-            printf("packet received from %d.%d.%d.%d %s \r\n",(int)hostAdress&0xFF000000>>24,
-            (int)hostAdress&0xFF0000>>16,
-            (int)hostAdress&0xFF00>>8,
-            (int)hostAdress&0xFF,
-            Downlink_packetBuffer);
-            #endif
             Json object((const char*) Downlink_packetBuffer, DOWNLINK_PACKET_SIZE);
-            string callbackJson;
             if(object.isValidJson()){
                 parseJSON(&object);
                 airtime = calculate_airtime(uplink_payload_size, uplink_DR, 1);
-                #ifdef DEBUG
-                printf("%d %d %s %s \r\n", 
+               // #ifdef DEBUG
+                printf("DR: %d PL-size: %d DevEUI: %s Payload: %s Header: %u Airtime: %ul\r\n", 
                 uplink_DR, 
                 uplink_payload_size, 
                 uplink_devEUI.c_str(), 
-                uplink_payload.c_str());
-                printf("Airtime: %ul \r\n", airtime);
-                #endif
-                
+                uplink_payload.c_str(),
+                uplink_payload_bytes[0],
+                airtime);
+                //#endif
+                watchdog.kick();
             }
             else{
                 Callback_timer.stop();
@@ -302,90 +409,29 @@ int main(void)
                 watchdog.kick();
                 continue;
             }
-            socket.flush();
-            #ifdef RTT_CORRECTION
-            int garbadge_length = (DOWNLINK_PACKET_SIZE+callbackJson.length())/2;
-            uint8_t garbadge[garbadge_length];
-            socket.beginPacket(HostIP, TX_RTT_port);
-            socket.write((const uint8_t*)garbadge,garbadge_length);
-            watchdog.kick();
-            if(!socket.endPacket()){
-                printf("something went wrong sending UDP RTT... \r\n");
-                Callback_timer.stop();
-                Callback_timer.reset();
-                socket.flush();
-                socket.close();
-                socket.begin(UL_port);
-                watchdog.kick();
-                continue;
+            switch (uplink_payload_bytes[0])
+            {
+            case SYNC:
+                synchronize_callback(HostIP);
+                break;
+            case EVENT:
+                event_callback(HostIP);
+                break;
+            
+            default:
+                break;
             }
-            RTT_timer.start();
-            socket.flush();
-            socket.close();
-            socket.begin(RX_RTT_port);
-            while(!socket.parsePacket());
-            socket.read(Downlink_packetBuffer, DOWNLINK_PACKET_SIZE);
-            socket.flush();
-            watchdog.kick();
-
-            RTT_timer.stop();
             Callback_timer.stop();
-            uint64_t timestamp = RTC_us.read_high_resolution_us()
-            + epoch_us;
-            uint32_t T_callback = Callback_timer.read_high_resolution_us() + RTT_timer.read_high_resolution_us() + ((uint32_t)uplink_GW_t*1000) + airtime;
-            callbackJson = createCallbackObject(timestamp, T_callback);
-            //#ifdef DEBUG
-            uint32_t temp_sec = timestamp / 1000000;
-            uint32_t temp_us = timestamp - ((uint64_t)temp_sec * 1000000);
-            uint16_t temp_ms = temp_us/1000;
-            temp_us = temp_us - (uint32_t)temp_ms*1000;
-            printf("Seconds since January 1, 1970 = %u ms: %u us: %u \r\n",
-            (unsigned int)temp_sec, (unsigned int)temp_ms, (unsigned int)temp_us);
-            printf("RTT: %d \r\n",RTT_timer.read_us());
-            printf("Tot callback time: %u us\r\n", T_callback);
-            //#endif
-            RTT_timer.reset();
-            #endif
-            watchdog.kick();
-            socket.beginPacket(HostIP, DL_port);
-            socket.write((const uint8_t*) callbackJson.c_str(), callbackJson.length());
-            if(!socket.endPacket()){
-                printf("something went wrong sending UDP Uplink... \r\n");
-                Callback_timer.stop();
-                Callback_timer.reset();
-                socket.flush();
-                socket.close();
-                socket.begin(UL_port);
-                watchdog.kick();
-                continue;
-            };
-            watchdog.kick();
-            socket.flush();
-            socket.close();
-            socket.begin(UL_port);
-            Callback_timer.stop();
-            #ifdef DEBUG
-            printf("Callback time: %d us\r\n", Callback_timer.read_us());
-            #endif
-            char buffer[32];
-            time_t seconds = time(NULL);
-            strftime(buffer, 32, "%H:%M:%S %p\n", localtime(&seconds));
-            printf("Uplink received from %s \r\n", uplink_devEUI.c_str());
-            printf("Time GMT0: %s \r", buffer);
             Callback_timer.reset();
         };
         watchdog.kick();
         if (print_time)
-        {
-            ouput_pin.write(1);
-    
-            uint64_t temp = RTC_us.read_high_resolution_us() + epoch_us;
+        {   
             uint32_t temp_sec = temp / 1000000;
             uint32_t temp_us = temp - ((uint64_t)temp_sec * 1000000);
             printf("Seconds since January 1, 1970 = %u, us: %u \r\n",
                    (unsigned int)temp_sec, (unsigned int)temp_us);
             wait_ms(10);
-            ouput_pin.write(0);
             print_time = false;
         };
  
